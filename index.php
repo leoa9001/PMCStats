9558b95b5b04aaeae221d9791e193559df7d76db
Justin Li <ml.justin.li@gmail.com>
2016-06-04 12:14:57 -0400

    Added stylesheet.
    Also added php to get data from the csv and store it in a 2D array...
    Maybe that's not what you want but oh whale.
    
    Shows table of first four rows of data (can change that), with
    cancer-causing formatting.

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<!-- Custom -->
+	<link type = "text/css" rel = "stylesheet" href = "css/stylesheet.css"/>
+</head>
+<body>
+	<h1> Super exciting website!</h1>
+	<?php
+	$data = array();
+	$file = fopen("data/PlayerStatData.csv", "r");
+	$count = 0;
+	while (!feof($file)) {
+				// $data[$count] = print_r(fgetcsv($file), true);
+		$data[$count] = fgetcsv($file);
+				// print_r(fgetcsv($file));
+				// echo "</br></br>";
+		$count++;
+	}
+	fclose($file);
+
+	echo "Show more rows by commenting out code in index.php...";
+	echo "</br>";
+	echo "<table>";
+	// for ($i = 0; $i < count($data); $i++) {
+	for ($i = 0; $i < 4; $i++) {
+		echo "<tr>";
+		for ($j = 0; $j < count($data[0]); $j++) {
+			echo "<td>";
+			echo $data[$i][$j];
+			echo "</td>";
+		}
+		echo "</tr>";
+	}
+	echo "</table>"
+	?>
+</body>
+</html>
