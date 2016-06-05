2848dc04d4b0385f786ace7d17d95257d67ee92d
Justin Li <mljustinli@leo-alcocks-MBP.fios-router.home>
2016-06-05 18:28:14 -0400

    Made some stat content jquery stuff to display different games/tabs in the player page.

--- js/stats_nav.js
+++ js/stats_nav.js
@@ -1,21 +1,26 @@
-$(document).keypress(function(e) {
-	if(e.which == 13) {
-		alert('You pressed enter!');
-	}
-});
-
-// $(document).click(function(e) {
-	
+// $(document).keypress(function(e) {
+// 	if(e.which == 13) {
+// 		alert('You pressed enter!');
+// 	}
 // });
 
-$(document).ready(function() {
+$(".stats-link").click(function(e) {
+	// alert(e.target.id);
+
 	// alert($("#stats-list .stats-link").length);
 	for (var i = 0; i < $("#stats-list .stats-link").length; i++) {
-		$(document).on("click", "stats-content" + i, function() {
-			alert("click on stats-content" + i);
-		});
+		if (i == parseInt(e.target.id)) {
+			// alert("something");
+			// alert($("#stats:nth-child(1)").id);
+			// alert($("#stats :nth-child(" + e.target.id + ")").id);
+			// $("#stats:nth-child(e.target.id)").removeClass("hide");
+			$("#stats #" + i).removeClass("hide");
+		} else {
+			// alert("something else");
+			// alert($("#stats:nth-child(1)").id);
+			// alert($("#stats :nth-child(" + e.target.id + ")").id);
+			// $("#stats:nth-child(e.target.id)").addClass("hide");
+			$("#stats #" + i).addClass("hide");
+		}
 	}
-	// $(document).on("click", "stats-content0", function() {
-	// 		alert("click on stats-content");
-	// });
-})
+});
