1113579befe8473a964d0f7b64af1900172833bf
Justin Li <mljustinli@leo-alcocks-MBP.fios-router.home>
2016-06-11 14:47:35 -0400

    Extracted player face from minecraft skin website and saved in img/heads

--- php/statsload.php
+++ php/statsload.php
@@ -1,4 +1,36 @@
 <?php
+/*
+Should check if stuff exists
+Does the player head loading thing requesting
+*/
+
+function grab_image($url,$saveto){
+    $ch = curl_init ($url);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_BINARYTRANSFER,1);
+    $raw=curl_exec($ch);
+    curl_close ($ch);
+    if(file_exists($saveto)){
+        unlink($saveto);
+    }
+    $fp = fopen($saveto,'x');
+    fwrite($fp, $raw);
+    fclose($fp);
+}
+
+grab_image("http://mcskinsearch.com/download/rio9001", "../img/heads/rio9001.png");
+
+$rect = array("x" => 8, "y" => 8, "width" => 8, "height" => 8);
+
+$img = imagecrop(imagecreatefrompng("../img/heads/rio9001.png"), $rect);
+imagepng($img, "../img/Heads/rio9001.png");
+
+
+/*
+Rest
+*/
+
 
 $playersName = $_GET['name'];
 
@@ -54,7 +86,6 @@ if(!$playerFound){
 for($i = 0; $i < count($colIndices);$i++){
 	echo "<p>". $colIndices[$i].": ".$playerData[$i]."</p>";
 }
-
 
 
 
